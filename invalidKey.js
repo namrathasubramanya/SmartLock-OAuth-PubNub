468b8b17ae7867dc7e34e2b75c1c7c52d95abed0
namrathasubramanya <namratha.subramanya@gmail.com>
2018-06-11 11:48:30 -0700

    publish subscribe

--- invalidKey.js
+++ invalidKey.js
@@ -1,7 +1,7 @@
 function unlock() {
-	var pubnub = new PubNub({
-    subscribeKey: 'sub-c-a1ad7926-5e0a-11e8-8ebf-f686a6d93a6b',
-    publishKey: 'pub-c-563cbabc-a399-4a76-830a-468a698efd4d',
+ var pubnub = new PubNub({
+    subscribeKey: 'Enter your subscribe key here',
+    publishKey: 'Enter your publish key here',
     authKey: 'invalid_key' 
   });
 

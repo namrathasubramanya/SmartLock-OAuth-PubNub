406b4e3821d1886dcc4c293bc68494f19c7f2090
namrathasubramanya <namratha.subramanya@gmail.com>
2018-06-11 11:35:57 -0700

    publish and subscribe key

--- validKey.js
+++ validKey.js
@@ -1,6 +1,6 @@
 var pubnub = new PubNub({
-    subscribeKey: 'sub-c-a1ad7926-5e0a-11e8-8ebf-f686a6d93a6b',
-    publishKey: 'pub-c-563cbabc-a399-4a76-830a-468a698efd4d'
+    subscribeKey: 'Enter your subscribe key here',
+    publishKey: 'Enter your publish key here'
 });
 
 pubnub.addListener({
